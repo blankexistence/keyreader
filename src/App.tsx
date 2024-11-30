7f70da968449e4ccc4b46e54a706e24a97e6d2a0
blankexistence <adib5413@gmail.com>
2024-11-30 02:29:04 +0000

    Code for resizeable code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 import { listen } from "@tauri-apps/api/event"
 import { appWindow } from "@tauri-apps/api/window"
 import Key from "./Key"
@@ -11,6 +11,8 @@ const metaKeys = ["MetaLeft", "MetaRight"]
 const altKeys = ["Alt", "AltGr"]
 
 function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+  const [maxChars, setMaxChars] = useState(2)
   const [alphabeticKeys, setAlphabeticKeys] = useState<string[]>(["NONE"])
   const [modifierKeys, setModifierKeys] = useState([
     {
@@ -42,8 +44,7 @@ function App() {
       specialKeysObj.Esc,
     ].includes(message)
 
-  const updateTickers = (message: string) => {
-    const max = 5
+  const updateTickers = (message: string, maxChars: number) => {
 
     setAlphabeticKeys((prevTickers) => {
       const charCode = message.charCodeAt(0)
@@ -84,7 +85,7 @@ function App() {
       }
 
       const currLen = newTickers.length
-      newTickers = currLen > max ? newTickers.slice(1) : newTickers
+      newTickers = currLen >= maxChars ? newTickers.slice(Math.floor(maxChars*0.2)) : newTickers
 
       return newTickers
     })
@@ -115,12 +116,13 @@ function App() {
       const { mode, message } = payload as { message: string; mode: string }
 
       if (mode === "Some") {
-        updateTickers(message)
+        updateTickers(message, maxChars)
+        updateModifierActiveStatus(message, true)
       }
 
       if (mode === "KeyPress") {
         updateControlPressedStatus(message, true)
-        updateTickers(message)
+        updateTickers(message, maxChars)
         updateModifierActiveStatus(message, true)
       }
 
@@ -133,31 +135,47 @@ function App() {
     return () => {
       unlisten.then((stop) => stop())
     }
+  }, [windowWidth, maxChars])
+
+  useEffect(() => {
+    setMaxChars(Math.floor(windowWidth/30))
+  }, [windowWidth])
+  
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
   }, [])
 
   return (
     <div data-tauri-drag-region className="ticker-container group">
-      <button
-        onClick={closeWindow}
-        className="btn-close group-hover:opacity-100 "
-      >
-        ✕
-      </button>
-      <div className="tickers">
-        {alphabeticKeys.map((ticker, idx) => {
-          return <Key key={idx} ticker={ticker} />
-        })}
-      </div>
-      <div className="modifier-keycaps">
-        {modifierKeys.map((keycap, index) => {
-          const active = keycap.active
-          return (
-            <div key={index} className={`keycap main-background`}>
-              <span className={active ? "" : "opacity-50"}>{keycap.title}</span>
-            </div>
-          )
-        })}
-      </div>
+        <button onClick={closeWindow} className="btn-close group-hover:opacity-100">
+          ✕
+        </button>
+        <div className={`tickers ${ windowWidth > 310 ? 'rounded-bl-3xl': '' }`}>
+          {alphabeticKeys.map((ticker, idx) => {
+            return <Key key={idx} ticker={ticker} />
+          })}
+        </div>
+        <div className="ml-auto">
+          <div className="modifier-keycaps w-[300px] max-w-[300px]">
+            {modifierKeys.map((keycap, index) => {
+              const active = keycap.active
+              return (
+                <div key={index} className={`keycap main-background`}>
+                  <span className={active ? "" : "opacity-50"}>{keycap.title}</span>
+                </div>
+              )
+            })}
+          </div>
+        </div>
     </div>
   )
 }
